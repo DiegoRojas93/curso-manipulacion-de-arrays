5acf0150f27de6b93d2ad332646d8d7e3e1bba47
Diego Rojas <diegorojas431@gmail.com>
2023-07-03 11:40:14 -0500

    concat

--- server/14-split.js
+++ server/14-split.js
@@ -1,6 +1,6 @@
 const text = `Curso de manipulacion de Arrays`
 
-const result = text.split(' ').join('-').toLowerCase()
+const result = text.replaceAll(' ','-').toLowerCase()
 
 console.log('URL: ',result);
 // Output: URL:  curso-de-manipulacion-de-Arrays
---
+++ server/15-concat.js
@@ -0,0 +1,7 @@
+const arr1 = ['D', 'i', 'e'],
+      arr2 = ['g', 'o'];
+
+const result = arr1.concat(arr2)
+
+console.log(result);
+// Output: [ 'D', 'i', 'e', 'g', 'o' ]
